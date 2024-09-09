97e73eddb73ac538fc25847630c96495f0ac30df
Rannyer <rdesenhos@gmail.com>
2024-09-09 17:44:44 -0300

    Inicio Aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DragDropListComponent } from "./components/drag-drop-list/drag-drop-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DragDropListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/drag-drop-list/drag-drop-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+import { NgFor } from '@angular/common';
+@Component({
+  selector: 'app-drag-drop-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './drag-drop-list.component.html',
+  styleUrl: './drag-drop-list.component.css'
+})
+export class DragDropListComponent {
+  
+}
